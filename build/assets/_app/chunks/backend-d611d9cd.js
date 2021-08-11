89f198846fc98f54b67b6bc877879a4d7dfc7aea
Steven Vroom <steven.klaas.vroom@gmail.com>
2021-08-11 16:48:37 +0200

    Remove build folder

--- build/assets/_app/chunks/backend-d611d9cd.js
+++
@@ -1,1 +0,0 @@
-const t="https://www.svspectrum.nl/backend/";async function n(n){return fetch(`${t}${n}`)}export{t as b,n as f};
--- build/assets/_app/pages/login.svelte-deed8d69.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as r,s as t}from"../chunks/vendor-800092b8.js";class a extends s{constructor(s){super(),r(this,s,null,null,t,{})}}export{a as default};
