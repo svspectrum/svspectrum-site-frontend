19b4d0033130d2bcfd081829dd4823166fc0325a
SarahNussler <97446513+SarahNussler@users.noreply.github.com>
2023-11-08 14:17:01 +0100

    Update Welcome.svelte

--- src/lib/components/Welcome.svelte
+++ src/lib/components/Welcome.svelte
@@ -1,25 +1,34 @@
 <script lang="ts">
+    // Importeer de dayjs-bibliotheek voor datum- en tijdmanipulatie.
     import dayjs from "dayjs";
+    // Importeer het "Doodles" component.
     import Doodles from "./Doodles.svelte";
 
+    // Bepaal de huidige maand met behulp van dayjs.
     let month = dayjs().month();
 </script>
 
 <div>
+    <!-- Inclusie van het "Doodles" component met één "amount" prop. -->
     <Doodles amount={1}/>
     <h1>
         {#if (month >= 5 && month <= 7)}
+            <!-- Conditioneel blok op basis van de huidige maand (mei t/m juli). -->
              Ga jij Bèta-gamma of FPS doen?
+             <!-- Een link naar de "Eerstejaarsdagen" als aan de voorwaarde is voldaan. -->
              <a href="/Eerstejaarsdagen">Kom naar onze eerstejaarsdagen!</a>
         {:else}
-            Welkom bij studie vereniging spectrum!
-            <a href="/Word-Lid">Wil je Lid worden?</a>
+            <!-- Alternatief bericht als de maand niet in mei t/m juli valt. -->
+            Welkom bij studievereniging spectrum!
+            <!-- Een link om lid te worden als de voorwaarde niet wordt voldaan. -->
+            <a href="/Word-Lid">Wil je lid worden?</a>
         {/if}
     </h1>
 </div>
 
 <style lang="scss">
     div {
+        /* Stel de hoogte van het welkomstgedeelte in met behulp van CSS-variabelen. */
         --welcome-height: calc(100vh - max(25vw, 40vh) - var(--header-height) - 2rem - 12px);
         display: flex;
         position: relative;
@@ -34,6 +43,7 @@
     }
 
     a {
+        /* Zorg ervoor dat links inline-blokken zijn voor een juiste weergave. */
         display: inline-block;
     }
 </style>
